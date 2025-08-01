37dc90349dc7a85a2cfe45ff0371da3c161351f1
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-08-01 10:48:45 +0200

    Delete django-models/LibraryProject/LibraryProject/forms.py
    forms deleted

--- django-models/LibraryProject/LibraryProject/forms.py
+++
@@ -1,9 +0,0 @@
-# bookshelf/forms.py
-
-from django import forms
-from .models import Book  # Assuming you have a Book model
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'description']  # Include fields you want to expose
