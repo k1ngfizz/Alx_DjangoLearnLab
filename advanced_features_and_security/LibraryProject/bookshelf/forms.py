8c4ac8c81ec00ffe344d1d96d88cbe4ff1fef00a
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-08-01 11:01:53 +0200

    Update forms.py
    Add ExampleForm as required by checker

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,9 +1,6 @@
-# bookshelf/forms.py
-
 from django import forms
-from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'description']  # adjust fields as needed
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
