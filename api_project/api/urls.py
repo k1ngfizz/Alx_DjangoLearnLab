e85cdb1f2b1a77a7715865589bc546ad3d57e8a8
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-08-02 12:39:00 +0200

    Update urls.py
    updated urls

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -9,5 +9,15 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # ✅ This connects to your API app
+    path('api/', include('api.urls')),  
 ]
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),  # Existing ListAPIView
+    path('', include(router.urls)), 
