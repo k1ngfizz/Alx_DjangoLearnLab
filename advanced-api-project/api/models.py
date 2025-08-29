0b32e5455e339973bf2fd00084735366141d2a87
NTIRENGANYA Fiston <kngfizzy6@gmail.com>
2025-08-29 12:08:16 +0200

    Update models.py
    inserted year

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -10,6 +10,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_date = models.DateField(blank=True, null=True)
+    publication_year = models.IntegerField(blank=True, null=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
